0473a0b8515cb44c8efa5da24b47b34cb53911d1
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-12 23:25:10 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text("Hellooo Duniyaa")),
+      appBar: AppBar(title: Text("My App")),
+    ),
+  ));
+}
+
+//Android :- Material Theme
+//iOS :- Coopertino theme
