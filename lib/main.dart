6df6773378833cd5fb0b3e72c9f37d158d98cdb7
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-13 16:24:44 +0530

    Added container

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,21 @@ void main() {
             style: TextStyle(fontFamily: 'Roboto', fontStyle: FontStyle.italic),
           )), //Pre Build widgets
       body: Center(
-          child: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith((states) {
-          return Colors.brown[400];
-        })),
-        onLongPress: () => {print("I am long pressed")},
-        onPressed: () => {print("I am Pressed")},
-        // onHover: () => {Color},
-        child: const Text("Press Me !"),
-      )),
+          child: Container(
+              height: 50,
+              width: 200,
+              padding: EdgeInsets.all(5),
+              margin: EdgeInsets.all(80),
+              child: ElevatedButton(
+                style: ButtonStyle(backgroundColor:
+                    MaterialStateProperty.resolveWith((states) {
+                  return Colors.brown[400];
+                })),
+                onLongPress: () => {print("I am long pressed")},
+                onPressed: () => {print("I am Pressed")},
+                // onHover: () => {Color},
+                child: const Text("Press Me !"),
+              ))),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.brown[400],
         onPressed: () => {},
