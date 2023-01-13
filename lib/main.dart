eb1fe8c55182fdc7f9f85c0697bf899dd291fe34
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-13 11:27:48 +0530

    13 Jan 11:27 commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Center(child: Text("Hellooo Duniyaa")),
-      appBar: AppBar(title: Text("My App")),
+      appBar: AppBar(title: Text("My First App")), //Pre Build widgets
+      body: Center(
+          child: ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+          return Colors.black;
+        })),
+        onPressed: () => {print("I am Pressed")},
+        child: Text("Press Me !"),
+      )
+          // child: Text(
+          //   "Hellooo Duniyaaa",
+          //   textAlign: TextAlign.left,
+          //   style: TextStyle(
+          //       color: Colors.red[700],
+          //       fontWeight: FontWeight.w800,
+          //       fontSize: 30,
+          //       fontFamily: 'Roboto-Italic',
+          //       fontStyle: FontStyle.italic),
+          // ),
+          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Text("+"),
+      ),
     ),
   ));
 }
